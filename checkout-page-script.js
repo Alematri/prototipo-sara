0627b90206d7e1ced6c404ec03c896f13136ad13
Alematri <alessandromatricardi@gmail.com>
2024-07-21 22:54:34 +0200

    [test usabilità mod 2]

--- checkout-page-script.js
+++ checkout-page-script.js
@@ -1,61 +1,144 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('DOM completamente caricato e analizzato');
+  const confirmButton = document.querySelector('.confirm-btn');
+  const proceedCheckoutButton = document.getElementById('proceedCheckoutButton');
+  const continueShoppingButton = document.getElementById('continueShoppingButton');
+  const downloadButton = document.getElementById('downloadButton');
+  const videoButton = document.getElementById('videoButton');
+  const openAccessModalButton = document.getElementById('openAccessModalButton');
+  const registerButton = document.getElementById('registerButton');
+  const continueWithoutRegisteringButton = document.getElementById('continueWithoutRegisteringButton');
+  const backButton = document.getElementById('backButton');
+  const backButtonInRegisterModal = document.getElementById('backButtonInRegisterModal');
+  const loginButtonInAccessModal = document.getElementById('loginButtonInAccessModal');
+  const registerButtonInRegisterModal = document.getElementById('registerButtonInRegisterModal');
+  const loginWithGoogleButtonInAccessModal = document.getElementById('loginWithGoogleButtonInAccessModal');
+  const loginWithGoogleButtonInRegisterModal = document.getElementById('loginWithGoogleButtonInRegisterModal');
+  const goToCheckoutButton = document.getElementById('goToCheckoutButton');
+  const goToCheckoutButtonInRegister = document.getElementById('goToCheckoutButtonInRegister');
+  const infoModalCloseButton = document.getElementById('infoModalCloseButton');
+  const isLoggedIn = false;
 
   const optionButtons = document.querySelectorAll('.option-btn');
-  const confirmButton = document.querySelector('.confirm-btn');
-  const confirmationModalElement = document.getElementById('confirmationModal');
-  const accountModalElement = document.getElementById('accountModal');
-  const accessModalElement = document.getElementById('accessModal');
-  const confirmationModal = new bootstrap.Modal(confirmationModalElement);
-  const accountModal = new bootstrap.Modal(accountModalElement);
-  const accessModal = new bootstrap.Modal(accessModalElement);
 
-  // Preselect "Alto"
-  const preselectedOption = document.querySelector('.option-btn.selected');
-
-  // Add event listener to option buttons
   optionButtons.forEach(button => {
-    button.addEventListener('click', () => {
-      optionButtons.forEach(btn => btn.classList.remove('selected'));
-      button.classList.add('selected');
-    });
+      button.addEventListener('click', () => {
+          optionButtons.forEach(btn => btn.classList.remove('selected'));
+          button.classList.add('selected');
+      });
   });
 
-  // Add event listener to confirm button
   confirmButton.addEventListener('click', () => {
-    console.log('Conferma cliccato');
-    confirmationModal.show();
+      const confirmationModal = new bootstrap.Modal(document.getElementById('confirmationModal'));
+      confirmationModal.show();
   });
 
-  document.getElementById('continueShoppingButton').addEventListener('click', () => {
-    console.log('Continuare a comprare cliccato');
-    window.location.href = 'index.html';
+  proceedCheckoutButton.addEventListener('click', () => {
+      if (isLoggedIn) {
+          const welcomeModal = new bootstrap.Modal(document.getElementById('welcomeModal'));
+          welcomeModal.show();
+      } else {
+          const accountModal = new bootstrap.Modal(document.getElementById('accountModal'));
+          accountModal.show();
+      }
   });
 
-  document.getElementById('proceedCheckoutButton').addEventListener('click', () => {
-    console.log('Procedi con il checkout cliccato');
-    confirmationModal.hide();  // Hide confirmation modal
-    accountModal.show();       // Show account modal
+  continueShoppingButton.addEventListener('click', () => {
+      const confirmationModal = bootstrap.Modal.getInstance(document.getElementById('confirmationModal'));
+      confirmationModal.hide();
+      window.location.href = 'index.html';
   });
 
-  document.getElementById('openAccessModalButton').addEventListener('click', () => {
-    console.log('Accedi cliccato');
-    accountModal.hide(); // Hide the account modal
-    accessModal.show(); // Show the access modal
+  openAccessModalButton.addEventListener('click', () => {
+      const accountModal = bootstrap.Modal.getInstance(document.getElementById('accountModal'));
+      accountModal.hide();
+      const accessModal = new bootstrap.Modal(document.getElementById('accessModal'));
+      accessModal.show();
   });
 
-  document.getElementById('loginButtonInAccessModal').addEventListener('click', () => {
-    console.log('Accedi cliccato nella modale di accesso');
-    // Handle login action here
+  registerButton.addEventListener('click', () => {
+      const accountModal = bootstrap.Modal.getInstance(document.getElementById('accountModal'));
+      accountModal.hide();
+      const registerModal = new bootstrap.Modal(document.getElementById('registerModal'));
+      registerModal.show();
   });
 
-  document.getElementById('registerButtonInAccessModal').addEventListener('click', () => {
-    console.log('Accedi con Google cliccato nella modale di accesso');
-    // Handle registration action here
+  continueWithoutRegisteringButton.addEventListener('click', () => {
+      const accountModal = bootstrap.Modal.getInstance(document.getElementById('accountModal'));
+      accountModal.hide();
+      alert('COMPLIMENTI, TEST COMPLETATO!');
   });
 
-  document.getElementById('continueWithoutRegisteringButtonInAccessModal').addEventListener('click', () => {
-    console.log('Continua senza registrarti cliccato nella modale di accesso');
-    window.location.href = 'checkout-page.html'; // Proceed to checkout
+  backButton.addEventListener('click', () => {
+      const accessModal = bootstrap.Modal.getInstance(document.getElementById('accessModal'));
+      accessModal.hide();
+      const accountModal = new bootstrap.Modal(document.getElementById('accountModal'));
+      accountModal.show();
+  });
+
+  backButtonInRegisterModal.addEventListener('click', () => {
+      const registerModal = bootstrap.Modal.getInstance(document.getElementById('registerModal'));
+      registerModal.hide();
+      const accountModal = new bootstrap.Modal(document.getElementById('accountModal'));
+      accountModal.show();
+  });
+
+  loginButtonInAccessModal.addEventListener('click', () => {
+      const accessModal = bootstrap.Modal.getInstance(document.getElementById('accessModal'));
+      accessModal.hide();
+      const welcomeModal = new bootstrap.Modal(document.getElementById('welcomeModal'));
+      welcomeModal.show();
+  });
+
+  registerButtonInRegisterModal.addEventListener('click', () => {
+      const registerModal = bootstrap.Modal.getInstance(document.getElementById('registerModal'));
+      registerModal.hide();
+      const confirmRegisterModal = new bootstrap.Modal(document.getElementById('confirmRegisterModal'));
+      confirmRegisterModal.show();
+  });
+
+  loginWithGoogleButtonInAccessModal.addEventListener('click', () => {
+      const accessModal = bootstrap.Modal.getInstance(document.getElementById('accessModal'));
+      accessModal.hide();
+      const welcomeModal = new bootstrap.Modal(document.getElementById('welcomeModal'));
+      welcomeModal.show();
+  });
+
+  loginWithGoogleButtonInRegisterModal.addEventListener('click', () => {
+      const registerModal = bootstrap.Modal.getInstance(document.getElementById('registerModal'));
+      registerModal.hide();
+      const confirmRegisterModal = new bootstrap.Modal(document.getElementById('confirmRegisterModal'));
+      confirmRegisterModal.show();
+  });
+
+  goToCheckoutButton.addEventListener('click', () => {
+      const welcomeModal = bootstrap.Modal.getInstance(document.getElementById('welcomeModal'));
+      welcomeModal.hide();
+      alert('COMPLIMENTI, TEST COMPLETATO!');
+  });
+
+  goToCheckoutButtonInRegister.addEventListener('click', () => {
+      const confirmRegisterModal = bootstrap.Modal.getInstance(document.getElementById('confirmRegisterModal'));
+      confirmRegisterModal.hide();
+      alert('COMPLIMENTI, TEST COMPLETATO!');
+  });
+
+  const showModalWithContent = (title, content) => {
+      const infoModal = new bootstrap.Modal(document.getElementById('infoModal'));
+      document.getElementById('infoModalTitle').innerText = title;
+      document.getElementById('infoModalContent').innerText = content;
+      infoModal.show();
+  };
+
+  downloadButton.addEventListener('click', () => {
+      showModalWithContent('Hai scaricato l\'App e il plantare consigliato per te è "Medio"', '');
+  });
+
+  videoButton.addEventListener('click', () => {
+      showModalWithContent('Hai guardato il video e il plantare consigliato per te è "Medio"', '');
+  });
+
+  infoModalCloseButton.addEventListener('click', () => {
+      const infoModal = bootstrap.Modal.getInstance(document.getElementById('infoModal'));
+      infoModal.hide();
   });
 });
